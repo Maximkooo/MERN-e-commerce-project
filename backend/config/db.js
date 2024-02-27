be6cae149b0ce80f8e6bac42869199e267fab4c1
Maksym Synytskyi <citomax7196@gmail.com>
2024-02-27 16:18:15 +0100

    user api

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_URI);
-		console.log(`Successfully connnected to mongoDB 👍`);
+		console.log(`Successfully connected to mongoDB 👍`);
 	} catch (error) {
 		console.error(`ERROR: ${error.message}`);
 		process.exit(1);
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 // Utiles
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -16,5 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
