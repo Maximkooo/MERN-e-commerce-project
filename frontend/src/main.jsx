15cd7899bc421ac35a1a4f4b23ba499532d06855
VladysslavTkach <tkachacc.program@gmail.com>
2024-03-04 11:43:25 +0100

    adding cart and shop pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,32 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import { Route, RouterProvider, createRoutesFromElements } from "react-router";
-import { createBrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { Route, RouterProvider, createRoutesFromElements } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
 
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from './components/PrivateRoute';
 
 // Auth
-import Login from "./pages/Auth/Login";
+import Login from './pages/Auth/Login';
 
-import AdminRoute from "./pages/Admin/AdminRoute";
-import Profile from "./pages/User/Profile";
-import UserList from "./pages/Admin/UserList";
-import Register from "./pages/Auth/Register";
-import CategoryList from "./pages/Admin/CategoryList";
+import AdminRoute from './pages/Admin/AdminRoute';
+import Profile from './pages/User/Profile';
+import UserList from './pages/Admin/UserList';
+import Register from './pages/Auth/Register';
+import CategoryList from './pages/Admin/CategoryList';
 
-import ProductList from "./pages/Admin/ProductList";
-import AllProducts from "./pages/Admin/AllProducts";
-import ProductUpdate from "./pages/Admin/ProductUpdate";
+import ProductList from './pages/Admin/ProductList';
+import AllProducts from './pages/Admin/AllProducts';
+import ProductUpdate from './pages/Admin/ProductUpdate';
 
-import Home from "./pages/Home.jsx";
-import Favorites from "./pages/Products/Favorites.jsx";
-import ProductDetails from "./pages/Products/ProductDetails.jsx";
+import Home from './pages/Home.jsx';
+import Favorites from './pages/Products/Favorites.jsx';
+import ProductDetails from './pages/Products/ProductDetails.jsx';
+
+import Cart from './pages/Cart.jsx';
+import Shop from './pages/Shop.jsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -31,8 +34,10 @@ const router = createBrowserRouter(
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
 			<Route index={true} path="/" element={<Home />} />
-      <Route path="/favorite" element={<Favorites />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
+			<Route path="/favorite" element={<Favorites />} />
+			<Route path="/product/:id" element={<ProductDetails />} />
+			<Route path="/cart" element={<Cart />} />
+			<Route path="/shop" element={<Shop />} />
 
 			{/* Registered users */}
 			<Route path="" element={<PrivateRoute />}>
@@ -43,15 +48,15 @@ const router = createBrowserRouter(
 				<Route path="userlist" element={<UserList />} />
 				<Route path="categorylist" element={<CategoryList />} />
 				<Route path="productlist" element={<ProductList />} />
-        <Route path="allproductslist" element={<AllProducts />} />
-        <Route path="productlist/:pageNumber" element={<ProductList />} />
-        <Route path="product/update/:_id" element={<ProductUpdate />} />
+				<Route path="allproductslist" element={<AllProducts />} />
+				<Route path="productlist/:pageNumber" element={<ProductList />} />
+				<Route path="product/update/:_id" element={<ProductUpdate />} />
 			</Route>
 		</Route>
 	)
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
 		<RouterProvider router={router} />
 	</Provider>
