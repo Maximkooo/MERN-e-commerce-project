2fa929fd9d324a282d7522953bf9a1682fd4fe68
Maksym Synytskyi <citomax7196@gmail.com>
2024-03-06 13:02:29 +0100

    update product page

--- frontend/src/pages/Products/ProductDetails.jsx
+++ frontend/src/pages/Products/ProductDetails.jsx
@@ -1,9 +1,13 @@
-import { useGetTopProductsQuery } from "../../redux/api/productApiSlice";
+import { useState } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import {
+  useGetProductDetailsQuery,
+  useCreateReviewMutation,
+} from "../../redux/api/productApiSlice";
+import Loader from "../../components/Loader";
 import Message from "../../components/Message";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import moment from "moment";
 import {
   FaBox,
   FaClock,
@@ -11,100 +15,174 @@ import {
   FaStar,
   FaStore,
 } from "react-icons/fa";
+import moment from "moment";
+import HeartIcon from "./HeartIcon";
+import Ratings from "./Ratings";
+import ProductTabs from "./ProductTabs";
+import { addToCart } from "../../redux/features/cart/cartSlice";
 
-const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+const ProductDetails = () => {
+  const { id: productId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
+  const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+
+  const {
+    data: product,
+    isLoading,
+    refetch,
+    error,
+  } = useGetProductDetailsQuery(productId);
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [createReview, { isLoading: loadingProductReview }] =
+    useCreateReviewMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createReview({
+        productId,
+        rating,
+        comment,
+      }).unwrap();
+      refetch();
+      toast.success("Review created successfully");
+    } catch (error) {
+      toast.error(error?.data || error.message);
+    }
+  };
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
   };
 
   return (
-    <div className="mb-4 lg:block xl:block md:block">
-      {isLoading ? null : error ? (
+    <>
+      <div>
+        <Link
+          to="/"
+          className="text-white font-semibold hover:underline ml-[10rem]"
+        >
+          Go Back
+        </Link>
+      </div>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
         <Message variant="danger">
-          {error?.data?.message || error.error}
+          {error?.data?.message || error.message}
         </Message>
       ) : (
-        <Slider
-          {...settings}
-          className="xl:w-[50rem]  lg:w-[50rem] md:w-[56rem] sm:w-[40rem] sm:block"
-        >
-          {products.map(
-            ({
-              image,
-              _id,
-              name,
-              price,
-              description,
-              brand,
-              createdAt,
-              numReviews,
-              rating,
-              quantity,
-              countInStock,
-            }) => (
-              <div key={_id}>
-                <img
-                  src={image}
-                  alt={name}
-                  className="w-full rounded-lg object-cover h-[30rem]"
-                />
+        <>
+          <div className="flex flex-wrap relative items-between mt-[2rem] ml-[10rem]">
+            <div>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="w-full xl:w-[50rem] lg:w-[45rem] md:w-[30rem] sm:w-[20rem] mr-[2rem]"
+              />
 
-                <div className="mt-4 flex justify-between">
-                  <div className="one">
-                    <h2>{name}</h2>
-                    <p> $ {price}</p> <br /> <br />
-                    <p className="w-[25rem]">
-                      {description.substring(0, 170)} ...
-                    </p>
-                  </div>
+              <HeartIcon product={product} />
+            </div>
 
-                  <div className="flex justify-between w-[20rem]">
-                    <div className="one">
-                      <h1 className="flex items-center mb-6">
-                        <FaStore className="mr-2 text-white" /> Brand: {brand}
-                      </h1>
-                      <h1 className="flex items-center mb-6">
-                        <FaClock className="mr-2 text-white" /> Added:{" "}
-                        {moment(createdAt).fromNow()}
-                      </h1>
-                      <h1 className="flex items-center mb-6">
-                        <FaStar className="mr-2 text-white" /> Reviews:
-                        {numReviews}
-                      </h1>
-                    </div>
+            <div className="flex flex-col justify-between">
+              <h2 className="text-2xl font-semibold">{product.name}</h2>
+              <p className="my-4 xl:w-[35rem] lg:w-[35rem] md:w-[30rem] text-[#B0B0B0]">
+                {product.description}
+              </p>
 
-                    <div className="two">
-                      <h1 className="flex items-center mb-6">
-                        <FaStar className="mr-2 text-white" /> Ratings:{" "}
-                        {Math.round(rating)}
-                      </h1>
-                      <h1 className="flex items-center mb-6">
-                        <FaShoppingCart className="mr-2 text-white" /> Quantity:{" "}
-                        {quantity}
-                      </h1>
-                      <h1 className="flex items-center mb-6">
-                        <FaBox className="mr-2 text-white" /> In Stock:{" "}
-                        {countInStock}
-                      </h1>
-                    </div>
-                  </div>
+              <p className="text-5xl my-4 font-extrabold">$ {product.price}</p>
+
+              <div className="flex items-center justify-between w-[20rem]">
+                <div className="one">
+                  <h1 className="flex items-center mb-6">
+                    <FaStore className="mr-2 text-white" /> Brand:{" "}
+                    {product.brand}
+                  </h1>
+                  <h1 className="flex items-center mb-6 w-[20rem]">
+                    <FaClock className="mr-2 text-white" /> Added:{" "}
+                    {moment(product.createAt).fromNow()}
+                  </h1>
+                  <h1 className="flex items-center mb-6">
+                    <FaStar className="mr-2 text-white" /> Reviews:{" "}
+                    {product.numReviews}
+                  </h1>
+                </div>
+
+                <div className="two">
+                  <h1 className="flex items-center mb-6">
+                    <FaStar className="mr-2 text-white" /> Ratings: {rating}
+                  </h1>
+                  <h1 className="flex items-center mb-6">
+                    <FaShoppingCart className="mr-2 text-white" /> Quantity:{" "}
+                    {product.quantity}
+                  </h1>
+                  <h1 className="flex items-center mb-6 w-[10rem]">
+                    <FaBox className="mr-2 text-white" /> In Stock:{" "}
+                    {product.countInStock}
+                  </h1>
                 </div>
               </div>
-            )
-          )}
-        </Slider>
+
+              <div className="flex justify-between flex-wrap">
+                <Ratings
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+
+                {product.countInStock > 0 && (
+                  <div>
+                    <select
+                      value={qty}
+                      onChange={(e) => setQty(e.target.value)}
+                      className="p-2 w-[6rem] rounded-lg text-black"
+                    >
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+              </div>
+
+              <div className="btn-container">
+                <button
+                  onClick={addToCartHandler}
+                  disabled={product.countInStock === 0}
+                  className="bg-pink-600 text-white py-2 px-4 rounded-lg mt-4 md:mt-0"
+                >
+                  Add To Cart
+                </button>
+              </div>
+            </div>
+
+            <div className="mt-[5rem] container flex flex-wrap items-start justify-between ml-[10rem]">
+              <ProductTabs
+                loadingProductReview={loadingProductReview}
+                userInfo={userInfo}
+                submitHandler={submitHandler}
+                rating={rating}
+                setRating={setRating}
+                comment={comment}
+                setComment={setComment}
+                product={product}
+              />
+            </div>
+          </div>
+        </>
       )}
-    </div>
+    </>
   );
 };
 
-export default ProductCarousel;
+export default ProductDetails;
