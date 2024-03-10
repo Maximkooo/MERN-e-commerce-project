3bf787cf19c723cbc704ea86f9d1c3940de2180e
Maksym Synytskyi <citomax7196@gmail.com>
2024-03-10 16:07:47 +0100

    Enable CORS with credentials in both frontend and backend.

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: [process.env.CLIENT_URL] }))
+app.use(cors({ credentials: true, origin: [process.env.CLIENT_URL] }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
